d6219d0e82878786b748745692aadacc570f2197
Ola <al.bednarczyk@gmail.com>
2020-02-24 14:56:20 +0100

    Fixed typo

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,7 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->chgiteck()) {
+        if (Auth::guard($guard)->check()) {
             return redirect()->route('companies.index');
         }
 
